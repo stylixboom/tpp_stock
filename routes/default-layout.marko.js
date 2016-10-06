21cd738acccf8d2263eb79579fac81fb93476541
Siriwat Kasamwattanarote <siriwat.k@sbibits.com>
2016-10-06 16:22:11 +0900

    Minor changes for Marko default-template

--- routes/default-layout.marko.js
+++ routes/default-layout.marko.js
@@ -17,7 +17,7 @@ function create(__helpers) {
     out.w("</title> <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"> <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\"> <!--[if lt IE 9]>\r\n      <script src=\"https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js\"></script>\r\n      <script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>\r\n    <![endif]--> <link rel=\"stylesheet\" href=\"/public/styles/classic.css\"> </head> <body> <script src=\"/socket.io/socket.io.js\"></script> <script src=\"/public/scripts/connector.js\"></script> <script src=\"/public/scripts/web_connector.js\"></script> <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script> <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script> <h1 if=\"data.showHeader !== false\"> ");
 
     layout_placeholder({
-        name: "title",
+        name: "header",
         content: data.layoutContent
       }, out);
 
