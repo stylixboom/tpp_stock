5a2c4cd5823bf201cacf4006dd1414ac99332a87
Siriwat Kasamwattanarote <siriwat.k@sbibits.com>
2016-10-06 16:20:38 +0900

    Correcting Mongoose data type

--- tpp_db_structure.js
+++ tpp_db_structure.js
@@ -5,47 +5,56 @@
 */
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-module.exports.stock = mongoose.model('stock', {
-	group_id: Integer,
-	product_id: Integer,
+module.exports.stock = mongoose.model('stock', new Schema({
+	group_id: Number,
+	product_id: Number,
 	transaction: [{
-		id: Integer,
-		time: Timestamp,
-		user_id: Integer,
-		customer_id: Integer,
+		id: Number,
+		time: Date,
+		user_id: Number,
+		customer_id: Number,
 		transaction_direction: String,	// Buying/Selling type
-		product_id: Integer,
-		unit_id: Integer,
-		unit_price: Double,				// Price per unit
-		amont: Integer,
+		product_id: Number,
+		unit_id: Number,
+		unit_price: Number,				// Price per unit
+		amont: Number,
 		payment: String,				// Cash/Cheque/Credit
 		status: String,					// Paid/Unpaid
 		remark: String					// Any note
 	}]
-});
+}, {
+	collection: 'stock'
+}));
 
-module.exports.group = mongoose.model('group', {
-	id: Integer,
+module.exports.product_group = mongoose.model('product_group', new Schema({
+	id: Number,
 	name: String,
 	description: String
-});
+}, {
+	collection: 'product_group'
+}));
 
-module.exports.product = mongoose.model('product', {
-	id: Integer,
+module.exports.product = mongoose.model('product', new Schema({
+	id: Number,
 	name: String,
 	description: String
-});
+}, {
+	collection: 'product'
+}));
 
-module.exports.unit = mongoose.model('unit', {
-	id: Integer,
+module.exports.unit = mongoose.model('unit', new Schema({
+	id: Number,
 	name: String,
-	converter_rate: Double,
+	converter_rate: Number,
 	description: String
-});
+}, {
+	collection: 'unit'
+}));
 
-module.exports.user = mongoose.model('user', {
-	id: Integer,
+module.exports.user = mongoose.model('user', new Schema({
+	id: Number,
 	name: String,
 	email: String,
 	username: String,
@@ -54,14 +63,18 @@ module.exports.user = mongoose.model('user', {
 	address: String,
 	description: String,
 	activated: Boolean					// Activated/Revoked
-});
+}, {
+	collection: 'user'
+}));
 
-module.exports.customer = mongoose.model('customer', {
-	id: Integer,
+module.exports.customer = mongoose.model('customer', new Schema({
+	id: Number,
 	name: String,
 	identity: String,
 	address: String,
 	description: String,
 	activated: Boolean					// Activated/Revoked
-});
+}, {
+	collection: 'customer'
+}));
 
