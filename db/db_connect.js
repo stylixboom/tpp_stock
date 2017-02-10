f19348eedd08a87f06e002a9eaf776d6c08f854e
Siriwat Kasamwattanarote <siriwat@live.jp>
2017-02-10 19:46:38 +0900

    Minor updates on db_connect

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,20 +1,22 @@
 // --------------- Generic function -----------------
 var utils = require(process.env.NODE_PATH + '/utils.js');
 
-var dbConfig = require('./db_config.js');
+var db_config = require('./db_config.js');
 var mongoose = require('mongoose');
-mongoose.connect(dbConfig.url);
+mongoose.connect(db_config.url);
 
 // Mongoose connection events
 {
     // When successfully connected
     mongoose.connection.on('connected', function () {
-        console.log(utils.printLogTime() + ' Mongoose connected to ' + dbConfig.url);
+        console.log(utils.printLogTime() + ' Mongoose connected to ' + db_config.url);
     });
 
     // If the connection throws an error
     mongoose.connection.on('error', function (err) {
         console.log(utils.printLogTime() + ' Mongoose connection error: ' + err);
+
+        process.exit(0);
     });
 
     // When the connection is disconnected
